4acdf4912834f99ae3e7d77ae93c812d8ea88f6c
huhu583 <3010494216@qq.com>
2020-07-12 20:43:04 +0800

    resct组件

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,4 +1,6 @@
 // webpack入口文件
+import React from "react"
+import ReactDOM from "react-dom"
 import data from './data.json'
 import '../CSS/index.less'
 console.log(data);
@@ -8,3 +10,7 @@ function add (x, y) {
     return x+y;
 }
 console.log(add(1,2));
+
+// 使用react创建虚拟DOM元素
+const helloWorld = React.createElement("div",null,"hello world");
+ReactDOM.render(helloWorld, document.getElementById("rq"));
