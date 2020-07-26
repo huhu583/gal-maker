7e148dada8c5ee9330044248703b8d21dc937ad8
yingyuan <907280636@qq.com>
2020-07-26 13:29:07 +0800

    07-26提交代码

---
+++ src/pages/GameSetting/GameSetting.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import cssObj from "@/pages/GameSetting/GameSetting.less"
+
+class GameSetting extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return <div>游戏列表设置</div>
+    }
+}
