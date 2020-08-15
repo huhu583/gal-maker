a9e9122296f6695db46199d29924e5e605a45f6a
huhu583 <3010494216@qq.com>
2020-08-15 17:49:46 +0800

    设计者主页面提交

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,13 +6,57 @@ class Header extends React.Component {
     constructor() {
         super();
 
-        this.state = {};
+        this.state = {
+            // 这里要区分是设计者的头部还是玩家的头部
+            headerType: 'designer',
+            // 表示当前选中的哪个页签
+            choseTag: 'index'
+        }
     }
 
     render() {
         return <div className={cssObj["header-container"]}>
-            
-        </div>;
+            {
+                this.state.headerType == 'player'?
+                <div>
+                    <div className={[cssObj["tag"], this.state.choseTag == 'index'?cssObj["chose-tag"]:null].join(' ')}>
+                        首页
+                    </div>
+                    <div className={cssObj["tag"]}>
+                        收藏夹
+                    </div>
+                    <div className={cssObj["tag"]}>
+                        团队管理
+                    </div>
+                    <div className={cssObj["tag"]}>
+                        留言板
+                    </div>
+                    <div className={cssObj["tag"]}>
+                        切换身份
+                    </div>
+                </div>
+                :
+                // 设计者头部
+                <div className={cssObj["content"]}>
+                    <div className={[cssObj["tag"], this.state.choseTag == 'index'?cssObj["chose-tag"]:null].join(' ')}>
+                        首页
+                    </div>
+                    <div className={[cssObj["tag"], this.state.choseTag == 'bookmark'?cssObj["chose-tag"]:null].join(' ')}>
+                        收藏夹
+                    </div>
+                    <div className={[cssObj["tag"], this.state.choseTag == 'teamManage'?cssObj["chose-tag"]:null].join(' ')}>
+                        团队管理
+                    </div>
+                    <div className={[cssObj["tag"], this.state.choseTag == 'questionArea'?cssObj["chose-tag"]:null].join(' ')}>
+                        问答专区
+                    </div>
+                    <div className={[cssObj["tag"], this.state.choseTag == 'changeUserType'?cssObj["chose-tag"]:null].join(' ')}>
+                        切换身份
+                    </div>
+                </div>
+            }
+            <div></div>
+        </div>
     }
 }
 
--- src/pages/DesignerMain/DesignerMain.jsx
+++ src/pages/DesignerMain/DesignerMain.jsx
@@ -1,16 +1,38 @@
 import React from "react"
 import Header from "@/components/Header/Header"
+import Bottom from "@/components/Bottom/Bottom"
 import cssObj from "@/pages/DesignerMain/DesignerMain.less"
 
 class DesignerMain extends React.Component {
     constructor() {
-        super();
+        super()
+
+        this.state = {
+            
+        }
     }
 
     render() {
         return <div>
             {/* 引入通用页面头,类型为设计者 */}
             <Header />
+
+            <div className={cssObj["game-project-content"]}>
+                <div className={cssObj["game-project-title"]}>
+                    <div>项目列表</div>
+                    <div>新建</div>
+                </div>
+                <div className={cssObj["game-project-search"]}>
+                    {/* 这里搜索框使用antd样式 */}
+                </div>
+
+                <div>
+
+                </div>
+            </div>
+
+            {/* 引入通用底部栏 */}
+            <Bottom />
         </div>
     }
 }
