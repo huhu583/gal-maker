2fa8bf07f68a045aba33e918387b068021e8d31f
huhu583 <3010494216@qq.com>
2020-08-26 21:24:55 +0800

    移除node_modules

--- src/pages/DesignerMain/DesignerMain.jsx
+++ src/pages/DesignerMain/DesignerMain.jsx
@@ -8,26 +8,46 @@ class DesignerMain extends React.Component {
         super()
 
         this.state = {
-            
+            gameList: [{
+                // 游戏ID
+                id: "1",
+                // 游戏名称
+                name: "galmaker-demo",
+                // 发布状态
+                isPublish: true,
+            }, {
+                // 游戏ID
+                id: "2",
+                // 游戏名称
+                name: "二次元神探",
+                // 发布状态
+                isPublish: false,
+            }],
         }
+
+
     }
 
     render() {
-        return <div>
+        return <div className={cssObj["game-designer"]}>
             {/* 引入通用页面头,类型为设计者 */}
             <Header />
 
-            <div className={cssObj["game-project-content"]}>
+            <div className={[cssObj["game-project-content"], 'area'].join(" ")}>
                 <div className={cssObj["game-project-title"]}>
                     <div>项目列表</div>
                     <div>新建</div>
                 </div>
                 <div className={cssObj["game-project-search"]}>
                     {/* 这里搜索框使用antd样式 */}
+                    <input type="text" />
                 </div>
-
-                <div>
-
+                <div className={cssObj["game-project-list"]}>
+                    {this.state.gameList.map(item => 
+                        <div className={cssObj["game-project-item"]} key={item.id}>
+                            {item.name}
+                        </div>
+                    )}
                 </div>
             </div>
 
